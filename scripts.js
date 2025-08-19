687d388fd1d9ac3c49cb8e5894c440c753d1bc8c
Tuchdov <tuchdov@gmail.com>
2025-08-19 18:18:21 +0300

    Finish writing the calcluation logic

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const add = function (a, b) {
-    return a + b;
+    aNum = Number(a)
+    bNum = Number(b)
+    return aNum + bNum;
 };
 
-const subtract = function (a, b) {
+const  subtract = function (a, b) {
     return a - b;
 };
 
@@ -18,8 +20,21 @@ const multiply = function (a, b) {
 // A calculator operation will consist of a number, an operator, and another number
 
 // create three variables one for each argument.
+let calculationFinished = false;
+const CalculateOperation = function (num1, op, num2) {
+    switch(op){
+        case '+':
+            return add(num1,num2);
 
-const CalculateOperation = function (num1, op, num2) { }
+        case '-':
+            return subtract(num1,num2);
+
+        case '*':
+            return multiply(num1,num2);
+
+        case '/':
+            return divide(num1,num2);
+ }};
 
 // create a digit and operator variables
 const digits = document.querySelectorAll('.digit');
@@ -30,7 +45,11 @@ let currentOperand = display.textContent;
 
 digits.forEach((digit) => {
     digit.addEventListener('click', () => {
-        if (display.textContent === '0') {
+        if (calculationFinished) {
+            display.textContent = digit.textContent;
+            calculationFinished = false
+        }
+        else if (display.textContent === '0') {
             display.textContent = digit.textContent;
             currentOperand = Number(display.textContent);
         }
@@ -47,7 +66,14 @@ let firstOperand = '';
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        if (chosenOperator === ''){
+        if (calculationFinished){
+            firstOperand = display.textContent;
+            currentOperand = '';
+            chosenOperator = operator.textContent;
+            display.textContent += ` ${chosenOperator} `;
+            calculationFinished = false;
+        }
+    else  if (chosenOperator === ''){
         firstOperand = currentOperand;
         currentOperand = '';
         chosenOperator = operator.textContent;
@@ -55,3 +81,17 @@ operators.forEach((operator) => {
 }
 // No 'else block here If there is already a chosen operation it will not switch to another
 );
+
+const equalBtn =  document.querySelector('#equal');
+equalBtn.addEventListener('click' ,()=> {
+    // if there are two operands and an operation.
+    secondOperand = display.textContent.split(' ')[2];
+    // calc function here 
+    answer = CalculateOperation(firstOperand, chosenOperator, secondOperand);
+    // display the result 
+    display.textContent = answer;
+    currentOperand = answer;
+    // reset the opereator
+    chosenOperator = ''
+    calculationFinished = true;
+})
