7e80ff626061fc5d3671a3a872237bfdc25c498a
Tuchdov <tuchdov@gmail.com>
2025-08-19 15:45:55 +0300

    Add first operand functionality

--- scripts.js
+++ scripts.js
@@ -1,24 +1,57 @@
-const add = function(a,b) {
-	return a + b;
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function(a,b) {
-	return a - b;
+const subtract = function (a, b) {
+    return a - b;
 };
 
 
-const divide = function(a,b) {
-	return a / b;
+const divide = function (a, b) {
+    return a / b;
 };
 
-const multiply = function(a,b) {
-	return a * b;
+const multiply = function (a, b) {
+    return a * b;
 };
 
 // A calculator operation will consist of a number, an operator, and another number
 
 // create three variables one for each argument.
-let num1 = 5;
-let num2 = 3;
-let op = '+';
-const CalculatorOperation = function(num1, op, num2) {
+
+const CalculateOperation = function (num1, op, num2) { }
+
+// create a digit and operator variables
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+
+const display = document.querySelector('#display');
+let currentOperand = display.textContent;
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        if (display.textContent === '0') {
+            display.textContent = digit.textContent;
+            currentOperand = Number(display.textContent);
+        }
+        else {
+            display.textContent += digit.textContent;
+            currentOperand = Number(display.textContent);
+        }
+    })
+});
+
+
+let chosenOperator = '';
+let firstOperand = '';
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (chosenOperator === ''){
+        firstOperand = currentOperand;
+        currentOperand = '';
+        chosenOperator = operator.textContent;
+        display.textContent += ` ${chosenOperator} `}})
+}
+// No 'else block here If there is already a chosen operation it will not switch to another
+);
