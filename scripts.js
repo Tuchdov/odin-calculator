6bf63001bf67aef2c91288e0d1569da403db7de4
Tuchdov <tuchdov@gmail.com>
2025-08-20 15:21:32 +0300

    Add delete feture with backspace key

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const add = function (a, b) {
     return aNum + bNum;
 };
 
-const  subtract = function (a, b) {
+const subtract = function (a, b) {
     return a - b;
 };
 
@@ -22,19 +22,20 @@ const multiply = function (a, b) {
 // create three variables one for each argument.
 let calculationFinished = false;
 const CalculateOperation = function (num1, op, num2) {
-    switch(op){
+    switch (op) {
         case '+':
-            return add(num1,num2);
+            return add(num1, num2);
 
         case '-':
-            return subtract(num1,num2);
+            return subtract(num1, num2);
 
         case '*':
-            return multiply(num1,num2);
+            return multiply(num1, num2);
 
         case '/':
-            return divide(num1,num2);
- }};
+            return divide(num1, num2);
+    }
+};
 
 // create a digit and operator variables
 const digits = document.querySelectorAll('.digit');
@@ -51,7 +52,7 @@ digits.forEach((digit) => {
         if (calculationFinished || display.textContent === '0') {
             display.textContent = digitText;
             calculationFinished = false; // Always reset the flag here
-        } 
+        }
         // Condition to ADD to the display
         else {
             display.textContent += digitText;
@@ -67,24 +68,26 @@ let firstOperand = '';
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        if (calculationFinished){
+        if (calculationFinished) {
             firstOperand = display.textContent;
             currentOperand = '';
             chosenOperator = operator.textContent;
             display.textContent += ` ${chosenOperator} `;
             calculationFinished = false;
         }
-    else  if (chosenOperator === ''){
-        firstOperand = currentOperand;
-        currentOperand = '';
-        chosenOperator = operator.textContent;
-        display.textContent += ` ${chosenOperator} `}})
+        else if (chosenOperator === '') {
+            firstOperand = currentOperand;
+            currentOperand = '';
+            chosenOperator = operator.textContent;
+            display.textContent += ` ${chosenOperator} `
+        }
+    })
 }
-// No 'else block here If there is already a chosen operation it will not switch to another
+    // No 'else block here If there is already a chosen operation it will not switch to another
 );
 
-const equalBtn =  document.querySelector('#equal');
-equalBtn.addEventListener('click' ,()=> {
+const equalBtn = document.querySelector('#equal');
+equalBtn.addEventListener('click', () => {
     // if there are two operands and an operation.
     secondOperand = display.textContent.split(' ')[2];
     // calc function here 
@@ -95,7 +98,7 @@ equalBtn.addEventListener('click' ,()=> {
     // reset the opereator
     chosenOperator = ''
     calculationFinished = true;
-})
+});
 
 // Adding a clear button
 const clearBtn = document.querySelector('#clear');
@@ -105,11 +108,36 @@ clearBtn.addEventListener('click', () => {
     chosenOperator = '';
     calculationFinished = false;
     currentOperand = '';
-})
+});
 
 // Adding a decimal point functionality
 const decimalPoint = document.querySelector('#decimal');
-decimalPoint.addEventListener('click', ()=> {
+decimalPoint.addEventListener('click', () => {
     if (display.textContent.split(' ').at(-1).includes('.') === false) // check if the last number, first or second has a dot in it.
-    display.textContent += decimalPoint.textContent;
-})
+        display.textContent += decimalPoint.textContent;
+});
+
+// Backspace to delete last digit or operand
+document.addEventListener('keydown', (event) => {
+    console.log("Backspace was pressed!");
+    if (event.key === 'Backspace') {
+        const lastChar = display.textContent.slice(-1);
+        const operations = ['+', '-', '*', '/'];
+        if (operations.includes(lastChar) ){
+            chosenOperator = '';
+            // display.textContent = display.textContent.slice(0, -2);
+        }
+       else if (lastChar === ' '){
+            // The operation is always beteen a white space before and after
+            // this will remove all the white space and the operation
+            display.textContent = display.textContent.slice(0, -3);
+            chosenOperator = '';
+        }
+        else {
+        display.textContent = display.textContent.slice(0, -1)};
+        // update the currentoperand to the new value only if its a number
+        if(operations.includes(display.textContent.slice(-1)) === false){
+             // sdisplay.textContent = display.textContent.slice(0, -1);
+             currentOperand = Number(display.textContent.split(' ').at(-1))
+    }
+}});
